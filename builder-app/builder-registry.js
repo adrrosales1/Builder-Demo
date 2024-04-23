e15539d336cca6fe98427370f5f31849f740eb54
Adrian Rosales <adrian@mints.cloud>
2024-04-22 21:49:49 -0600

    First

--- builder-app/builder-registry.js
+++ builder-app/builder-registry.js
@@ -1,14 +1,9 @@
 "use client";
 import { builder, Builder } from "@builder.io/react";
 import Counter from "./components/Counter/Counter";
-import Counter2 from "./components/Counter/Counter2";
 
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
 Builder.registerComponent(Counter, {
   name: "Counter",
-});
-
-Builder.registerComponent(Counter2, {
-  name: "Counter2",
 });
